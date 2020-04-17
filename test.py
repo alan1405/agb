b9a63678a38a0fa1f50d52e72353b2d92689d7be
alan <alan1405@yahoo.com>
2020-04-17 12:41:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -2,39 +2,90 @@
 import psycopg2
 import json
 import requests
+import datetime
+from xml.etree import ElementTree
+
 db_port = 5432
 db_name = "wmca"
-db_user = 
-db_pass = 
+db_user = "fme_server_write"
+db_pass = "tomcatFileScript43"
 
-def lambda_handler(event, context):
+def test():
     con = psycopg2.connect(
-            host = "wmcapostgres.cok4j10mlpxt.eu-west-1.rds.amazonaws.com",
-            database = db_name,
-            user = db_user,
-            password = db_pass)
-    r = requests.get('https://traffic.ls.hereapi.com/traffic/6.2/flow.json/?apiKey=vRbDFfFW5VMv9bBcbjKDgRvwtQZO77W1RPZqaPE_kBI&bbox=52.6625,-1.4209;52.3465,-2.2072&locationreferences=shp')
-    theflow = json.loads(r.text)
-    i = theflow['RWS'][0]['RW']
-    for x in i:
-    	#print(x)
-    	geom = x['FIS'][0]['FI'][0]['SHP'][0]['value']
-    	fc = x['FIS'][0]['FI'][0]['SHP'][0]['FC']
-    	lid = x['FIS'][0]['FI'][0]['SHP'][0]['LID']
-    	le = x['FIS'][0]['FI'][0]['SHP'][0]['LE']
-    	fw = x['FIS'][0]['FI'][0]['SHP'][0]['FW']
-    	ty = x['FIS'][0]['FI'][0]['CF'][0]['TY']
-    	sp = x['FIS'][0]['FI'][0]['CF'][0]['SP']
-    	#print(x['FIS'][0]['FI'][0]['CF'][0]['SU'])
-    	ff = x['FIS'][0]['FI'][0]['CF'][0]['FF']
-    	jf = x['FIS'][0]['FI'][0]['CF'][0]['JF']
-    	cn = x['FIS'][0]['FI'][0]['CF'][0]['CN']
-    	mid = x['mid']
-    	pbt = x['PBT']
-    	
-    	cur = con.cursor()
-    	cur.execute("INSERT INTO wmca_road.wmca_here_traffic_flow(cn, fc, ff, fw, geom, jf, le, lid, mid, pbt, sp, su, ty) VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",(cn, fc, ff, fw, geom, jf, le, lid, mid, pbt, sp, 0, ty))
-    	con.commit()
+        host = "wmcapostgres.cok4j10mlpxt.eu-west-1.rds.amazonaws.com",
+        database = db_name,
+        user = db_user,
+        password = db_pass)
+    r = requests.get('https://publicdashacc.blob.core.windows.net/publicdata?restype=container&comp=list')
+    # root address for the JSON GeoJSON etc https://c19pub.azureedge.net/
+    #rtheflow = r.json()
+
+    date_now = datetime.datetime.now()
+    db_date = date_now.strftime("%d/%m/%Y %H:%M:%S")
+    #print(db_date)
+
+    root = ElementTree.fromstring(r.content)
+    #print(dir(root))
+    
+    jsonloc = []
+    for child in root.findall('.//Name'):
+        #print(child.text)
+        jsonloc.append(child.text)
+    jsonloc.sort(reverse=True)
+    
+    jsonloc.pop(0)
+    jsonloc.pop(0)
+    jsonloc.pop()
+    
+    jsonloc.sort(reverse=True)
+    
+    #get the lastest JSON file dataset
+    
+    latestcovid = jsonloc[0]
+    
+    #print(jsonloc)
+    rooturl = "https://c19pub.azureedge.net/"
+    regions = "regions.geojson"
+    countries = "countries.geojson"
+    utlas = "utlas.geojson"
+    drequest = rooturl+latestcovid
+    data1 = requests.get(drequest)
+    
+    
+    updated = datetime.datetime.now()
+    rdata = data1.json()
+    #print(rdata)
+    lupdated = rdata['lastUpdatedAt']
+    #print(rdata['overview']['K02000001']['totalCases'])
+    totalcases = rdata['overview']['K02000001']['totalCases']
+    newcases = rdata['overview']['K02000001']['newCases']
+    deaths = rdata['overview']['K02000001']['deaths']
+    name = rdata['overview']['K02000001']['name']
+    dailydeaths = rdata['overview']['K02000001']['dailyDeaths']
+    dailytotaldeaths = rdata['overview']['K02000001']['dailyTotalDeaths']
+
+    allcur = con.cursor()
+    allcur.execute("INSERT INTO wmca_covid_dailies.uk_covid_dailies(name, totalcases, newcases, deaths, dailydeaths, dailytotaldeaths, updated) VALUES( %s, %s, %s, %s, %s, %s, %s)",(json.dumps(name), json.dumps(totalcases), json.dumps(newcases), json.dumps(deaths), json.dumps(dailydeaths), json.dumps(dailytotaldeaths), lupdated ))
+    con.commit()
+    #print(rdata['utlas']['E08000025']['name'])
+    #print(rdata['utlas']['E08000025']['totalCases'])
+    #print(rdata['utlas']['E08000025']['dailyConfirmedCases'])
+    #print(rdata['utlas']['E08000025']['dailyTotalConfirmedCases'])
+    auths = ['E08000025','E08000026','E08000027','E08000028','E08000029','E08000030','E08000031']
+    
+    for auth in auths:
+        print(rdata['utlas'][auth]['name'])
+        print(rdata['utlas'][auth]['totalCases'])
+        authority = rdata['utlas'][auth]['name']
+        dailyconfirmedcases = rdata['utlas'][auth]['dailyConfirmedCases'] 
+        dailytotalconfirmedcases = rdata['utlas'][auth]['dailyTotalConfirmedCases'] 
+        totalcases = rdata['utlas'][auth]['totalCases']
+        cur = con.cursor()
+        cur.execute("INSERT INTO wmca_covid_dailies.covid_dailies_cases(authority, dailyconfirmedcases, dailytotalconfirmedcases, totalcases, updated) VALUES( %s, %s, %s, %s, %s)",(json.dumps(authority), json.dumps(dailyconfirmedcases), json.dumps(dailytotalconfirmedcases), json.dumps(totalcases), updated ))
+        con.commit()
+
     cur.close()
     con.close()
-    return theflow
+    return "DONE"
+
+print(test())
